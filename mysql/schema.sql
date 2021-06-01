ad00fd3e772a93dc8590b562edf52b4d9e71a933
Mudit Singh <muditsingh13@gmail.com>
2021-06-01 16:44:53 +0530

    Added Demo & Web Interface

---
+++ mysql/schema.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE fuzzycrawler_test;
+USE fuzzycrawler_test;
+CREATE TABLE `user`
+(
+  `id` bigint NOT NULL,
+  `fname` varchar
+(255) NOT NULL,
+  `lname` varchar
+(255) NOT NULL,
+  `email` varchar
+(400) NOT NULL,
+  `pass` varchar
+(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+ALTER TABLE `user`
+ADD PRIMARY KEY
+(`id`),
+ADD UNIQUE KEY `email`
+(`email`);
+ALTER TABLE `user`
+  MODIFY `id` bigint NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+INSERT INTO `user`
+  (`fname`,`lname
+  `,`email`,`pass`) VALUES
+('John','Doe','john.doe@example.com','Doe@123');
